19c5555a69627cb0ab3a19ff2af7d2f4ed7f1906
arvind-s <arvind.s01994@gmail.com>
2015-09-20 13:42:46 +0530

    indentation

--- my_maxent.py
+++ my_maxent.py
@@ -3,44 +3,46 @@ from scipy.optimize import minimize as mymin
 
 class MyMaxEnt(object):
 	'''
-	Python Class for the Maximum Entropy Classifier
+		Python Class for the Maximum Entropy Classifier
 	'''
 	def __init__(self,hist_list,feature_fn_list):
 		'''
-		Initialises the Max Ent model by producing the feature vectors for the training data
+			Initialises the Max Ent model by producing the feature vectors for the training data
 		'''
 		self.fvectors = self.create_dataset(hist_list)
 
 
 	def init_model(self):
 		'''
-		Initialises the model parameter
+			Initialises the model parameter
 		'''
 		self.model = np.array([0]*10)
 
 	def cost(self,model):
 		'''
-		Given the model, compute the cost 
+			Given the model, compute the cost 
 		'''
+		# return L(v)
 
 	def train(self):
 		'''
-		Train the classifier
+			Train the classifier
 		'''
-		params = mymin(self.cost, self.model, method = 'L-BFGS-B')
+		params = mymin(self.cost, self.model, method = 'L-BFGS-B', jac=gradient)
 		#self.model = 
 
 	def p_y_given_x(self,h,tag):
 		'''
-		Take the history tuple and the required tag as the input and return the probability
+			Take the history tuple and the required tag as the input and return the probability
 		'''
 
 	def classify(self,h):
 		'''
-		Performs the classification by determining the tag that maximizes the probability
+			Performs the classification by determining the tag that maximizes the probability
 		'''
 
 	def gradient(self):
 		'''
-		Maximizes the log-likelihood
+			Maximizes the log-likelihood(Minimizes the negative)
 		'''
+		
