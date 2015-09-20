3e359564891308e39780a8e6c62c4fe8307fb6f1
ashwin naresh <ashwin.naresh94@gmail.com>
2015-09-20 15:21:29 +0530

    added implementations of p(y|x;v), classify, load and save

--- my_maxent.py
+++ my_maxent.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.optimize import minimize as mymin 
+import pickle, math
 
 class MyMaxEnt(object):
 	'''
@@ -10,7 +11,7 @@ class MyMaxEnt(object):
 			Initialises the Max Ent model by producing the feature vectors for the training data
 		'''
 		self.fvectors = self.create_dataset(hist_list)
-
+		self.tagset = ["PERSON","ORGANIZATION","GPE","MONEY","DATE","TIME"]
 
 	def init_model(self):
 		'''
@@ -28,21 +29,45 @@ class MyMaxEnt(object):
 		'''
 			Train the classifier
 		'''
-		params = mymin(self.cost, self.model, method = 'L-BFGS-B', jac=gradient)
+		params = mymin(self.cost, self.model, method = 'L-BFGS-B', jac = gradient)
 		#self.model = 
 
 	def p_y_given_x(self,h,tag):
 		'''
 			Take the history tuple and the required tag as the input and return the probability
 		'''
+		return float(math.exp(np.dot(self.model,self.fvectors[(h,tag)])))/sum([math.exp(np.dot(self.model,self.fvectors[(h,tag)])) for tag in self.tagset])
+
 
 	def classify(self,h):
 		'''
 			Performs the classification by determining the tag that maximizes the probability
 		'''
+		max_prob = 0
+		best_tag = ""
+
+		for tag in self.tagset:
+			prob = self.p_y_given_x(h,tag)
+			if prob > max_prob:
+				max_prob = prob
+				best_tag = tag
+
+		return best_tag
+
 
 	def gradient(self):
 		'''
 			Maximizes the log-likelihood(Minimizes the negative)
 		'''
 		
+	def load(model_file):
+		'''
+			Load the Max Ent model from the model file
+		'''
+		self.model = pickle.load(open(model_file))
+
+	def save(model_file):
+		'''
+			Save the model 
+		'''
+		pickle.dump(self.model,open((model_file),"w"))
