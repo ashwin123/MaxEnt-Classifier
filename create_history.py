37add7d62626170b612458995a6fc9b67e4a82cd
Anish Narang <anish_an@hotmail.com>
2015-09-22 20:44:11 +0530

    Testing

--- create_history.py
+++ create_history.py
@@ -1,27 +1,34 @@
 import nltk
 
-def create_history(tags,tokens):
+def create_history(tag_dict):
     """Method to create history tuples"""
     history = []
-    for i in range(len(tokens)):
-        for j in range(len(tokens[i])):
+    for i in tag_dict.keys():
+        for j in range(len(tag_dict[i])):
             if((j-2)<0):
                 u = '*'
             else:
-                try:
-                    u = tags[tokens[i][j-2]]
-                except:
-                    u = 'OTHER'
+                u = tag_dict[i][j-2][1]
+
             if((j-1)<0):
                 v = '*'
             else:
-                try:
-                    v = tags[tokens[i][j-1]]
-                except:
-                    v ='OTHER'
-            x = (u,v,tuple(tokens[i]),j)
+                v = tag_dict[i][j-1][1]
+
+            x = (u,v,tuple(nltk.word_tokenize(i)),j)
             history.append(x)
     return history
+
+tagged_history=dict()
+
+def extract_entity_names(t,sent):
+    l=[]
+    for n in t:
+        if isinstance(n, nltk.tree.Tree):
+            l.append((str(n).split("/")[0].split(" ")[1],n.label()))
+        else:
+            l.append((n[0],'OTHER'))
+    tagged_history[sent]=l
 
 with open('apple-iphone-5-first-look.txt', 'r') as f:
     sample = f.read()
@@ -29,20 +36,21 @@ with open('apple-iphone-5-first-look.txt', 'r') as f:
 sentences = nltk.sent_tokenize(sample)
 tokenized_sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
 tagged_sentences = [nltk.pos_tag(sentence) for sentence in tokenized_sentences]
+#print tagged_sentences
 chunked_sentences = []
 for i in tagged_sentences:
     chunked_sentences.append(nltk.chunk.ne_chunk(i))
+#print chunked_sentences
 
-def extract_entity_names(t):
-    for n in t:
-        if isinstance(n, nltk.tree.Tree):
-                tags[str(n).split("/")[0].split(" ")[1]] = n.label()
 
 entity_names = []
 tags = dict()
+ctr=0
 for tree in chunked_sentences:
-    extract_entity_names(tree)
-h = create_history(tags,tokenized_sentences)
+    extract_entity_names(tree,sentences[ctr])
+    ctr+=1
+# print(tagged_history)
+h = create_history(tagged_history)
 f = open("history.txt","w")
 f.write(str(h))
 f.close()
--- ner_tag.py
+++ ner_tag.py
@@ -2,15 +2,92 @@ import numpy as np
 from scipy.optimize import minimize as mymin
 from my_maxent import MyMaxEnt
 import feature_functions as f_fn
-
-hist_list = eval(open('history.txt').read())
-feature_fn_list = [f_fn.f1,f_fn.f2,f_fn.f3,f_fn.f4,f_fn.f5,f_fn.f6,f_fn.f7,f_fn.f8,f_fn.f9,f_fn.f10]
-
-ner_tagger = MyMaxEnt(hist_list,feature_fn_list)
-#ner_tagger.train()
-#print ner_tagger.model
-ner_tagger = ner_tagger.load("ner_iphone5.txt")
-h = ('*', '*', ('Apple', 'iPhone', '5', ':', 'First', 'look', 'As', 'I', 'played', 'around', 'with', 'the', 'iPhone', '5', 'on', 'Wednesday', ',', 'I', 'wondered', 'what', 'the', 'late', 'Steve', 'Jobs', 'would', 'have', 'thought', 'about', 'the', 'latest', 'twist', 'on', 'Apple', "'s", 'best-selling', 'device.It', 'did', "n't", 'take', 'long', 'to', 'conclude', 'Jobs', 'would', 'have', 'been', 'delighted', 'with', 'the', 'iPhone', '5', "'s", 'blend', 'of', 'beauty', ',', 'utility', 'and', 'versatility.Add', 'in', 'the', 'more', 'advanced', 'technology', 'and', 'new', 'features', 'that', 'went', 'into', 'this', 'iPhone', ',', 'and', 'it', "'s", 'clear', 'Apple', 'has', 'come', 'up', 'with', 'another', 'product', 'that', 'will', 'compel', 'hordes', 'of', 'people', 'to', 'line', 'up', 'outside', 'its', 'stores', 'before', 'its', 'Sept.', '21', 'release', 'in', 'the', 'U.S.', ',', 'Japan', ',', 'Britain', ',', 'Germany', ',', 'France', 'and', 'four', 'other', 'countries', '.'), 0)
-print ner_tagger.classify(h)
+import nltk
 
 
+def extract_entity_names(t,sent):
+    l=[]
+    for n in t:
+        if isinstance(n, nltk.tree.Tree):
+            l.append((str(n).split("/")[0].split(" ")[1],n.label()))
+        else:
+            l.append((n[0],'OTHER'))
+    tagged_nltk[sent]=l
+
+
+def max_ent_test():
+    '''
+        Our test function. Stores result as {sentence:[(word,tag),(word,tag)..]}
+    '''
+    u='*'
+    v='*'
+    for i in range(len(tokenized_sentences)):
+        l=[]
+        for j in range(len(tokenized_sentences[i])):
+            if((j-2)<0 and (j-1)<0):
+                hist_tuple = ('*','*',tuple(tokenized_sentences[i]),0)
+                ner_tagger.fvectors[hist_tuple] = {}
+                for tag in ner_tagger.tags:
+                    ner_tagger.fvectors[hist_tuple][tag] = np.array([fun(hist_tuple,tag) for fun in feature_fn_list])
+
+                u=ner_tagger.classify(hist_tuple)
+                print(u)
+                l.append((tokenized_sentences[i][j],u))
+
+            elif ((j-2)<0 and (j-1)==0):
+                hist_tuple = ('*',u,tuple(tokenized_sentences[i]),1)
+                ner_tagger.fvectors[hist_tuple] = {}
+
+                for tag in ner_tagger.tags:
+                    ner_tagger.fvectors[hist_tuple][tag] = np.array([fun(hist_tuple,tag) for fun in feature_fn_list])
+                v=ner_tagger.classify(hist_tuple)
+                print(v)
+                l.append((tokenized_sentences[i][j],v))
+
+            else:
+                hist_tuple=(u,v,tuple(tokenized_sentences[i]),j)
+
+                ner_tagger.fvectors[hist_tuple] = {}
+                for tag in ner_tagger.tags:
+                    ner_tagger.fvectors[hist_tuple][tag] = np.array([fun(hist_tuple,tag) for fun in feature_fn_list])
+                prev=ner_tagger.classify(hist_tuple)
+                u=v
+                v=prev
+                print(u,v,prev)
+                l.append((tokenized_sentences[i][j],prev))
+            my_tagged_sent[sentences[i]]=l
+
+
+def nltk_tagger():
+    '''
+
+    '''
+    tagged_sentences = [nltk.pos_tag(sentence) for sentence in tokenized_sentences]
+    chunked_sentences = []
+    for i in tagged_sentences:
+        chunked_sentences.append(nltk.chunk.ne_chunk(i))
+    ctr=0
+    for tree in chunked_sentences:
+        extract_entity_names(tree,sentences[ctr])
+        ctr+=1
+
+
+if __name__ == "__main__":
+    tagged_nltk=dict()  # Result for our tagging
+    my_tagged_sent=dict() # Result of NLTK tagging
+
+    hist_list = eval(open('history.txt').read())
+    feature_fn_list = [f_fn.f1,f_fn.f2,f_fn.f3,f_fn.f4,f_fn.f5,f_fn.f6,f_fn.f7,f_fn.f8,f_fn.f9,f_fn.f10]
+
+    ner_tagger = MyMaxEnt(hist_list,feature_fn_list)
+    ner_tagger.train()
+    # test_sample = eval(open("test_sentences.txt")).read()
+    test_sample=" ".join(['Apple', 'iPhone', 'is', 'a', 'new', 'addition', 'to', 'the', 'Apple','family','.'])
+    sentences=nltk.sent_tokenize(test_sample)
+    tokenized_sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
+
+    nltk_tagger()
+    max_ent_test()
+
+    print "Our:",my_tagged_sent
+    print "Their:",tagged_nltk
